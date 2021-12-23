8c18d1c7143d10357ca969bf694456d07dad2065
Александр <sashaneiv01@yandex.ru>
2021-12-23 22:25:10 +0300

    Commit 2

--- src/ru/skypro/Employee.java
+++ src/ru/skypro/Employee.java
@@ -1,24 +1,51 @@
 package ru.skypro;
 
-public class Employee {
+import com.sun.org.apache.xml.internal.dtm.ref.sax2dtm.SAX2RTFDTM;
 
+public class Employee {
+    int salary;
+    public String department;
+    public EmployeeList employeeList;
     public static int employeeCount = 0;
-    public Employee()
-    {
+
+    public Employee(EmployeeList employeeList, String department, int salary) {
+        this.department = department;
+        this.salary = salary;
+        this.employeeList = employeeList;
         Employee.employeeCount++;
     }
-//    int id;
+
+    public String getDepartment() {
+        return getDepartment();
+    }
+
+    public EmployeeList getEmployeeList() {
+        return employeeList;
+    }
+
+    public int getSalary() {
+        return getSalary();
+    }
+
+    @Override
+    public String toString() {
+        return employeeList +
+                " зарплата - " + salary +
+                " отдел в котором работает - " + department;
+    }
+
+    //    int id;
 //    int i = id++;
 
-    public static int getEmployeeCount()
-    {
-        return Employee.employeeCount;
-
-    }
-
-    public static void setEmployeeCount(int employeeCount)
-    {
-        Employee.employeeCount = employeeCount;
-
-    }
+//    public static int getEmployeeCount()
+////    {
+////        return Employee.employeeCount;
+////
+////    }
+////
+////    public static void setEmployeeCount(int employeeCount)
+////    {
+////        Employee.employeeCount = employeeCount;
+////
+////    }
 }
--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,9 +3,28 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-        EmployeeList Volkov = new EmployeeList
-                ("Волков", "Александр", "Александрович");
-        Employee[] employees = new Employee[10];
-        System.out.println(Volkov);
+        EmployeeList Volkov = new EmployeeList("Волков", "Александр", "Александрович");
+        EmployeeList Volkova = new EmployeeList("Волкова", "Дарья", "Игоревна");
+
+        Employee employee1 = new Employee(Volkov, "1", 15000);
+        Employee employee2 = new Employee(Volkova, "2", 14000);
+        Employee[] newEmployees = new Employee[10];
+//        Employee newEmployee = new Employee(Volkov, "1", 15000);
+
+        addEmployee(newEmployees, employee1);
+        addEmployee(newEmployees, employee2);
+        System.out.println(Employee.employeeCount);
+        System.out.println(newEmployees[1]);
+
+
+    }
+    private static boolean addEmployee (Employee[] newEmployees, Employee employee) {
+        for (int i = 0; i < newEmployees.length; i++) {
+            if (newEmployees[i] == null) {
+                newEmployees[i] = employee;
+                return true;
+            }
+        }
+        return false;
     }
 }
